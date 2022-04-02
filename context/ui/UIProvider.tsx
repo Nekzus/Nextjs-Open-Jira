f05e703dd4788847e18ba3ae4671e79656a72127
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-04-02 16:43:22 -0300

    Implementacion Reducer

---
+++ context/ui/UIProvider.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useReducer } from 'react';
+import { UIContext, uiReducer } from './';
+
+export interface UIState {
+    sidemenuOpen: boolean;
+}
+
+const UI_INITIAL_STATE: UIState = {
+    sidemenuOpen: false,
+};
+
+export const UIProvider: FC = ({ children }) => {
+
+    const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE);
+
+    const openSideMenu = () => {
+        dispatch({ type: 'UI - Open Sidebar' });
+    };
+
+    // closeSideMenu
+    const closeSideMenu = () => dispatch({ type: 'UI - Close Sidebar' });
+    
+
+    return (
+        <UIContext.Provider
+        value={{
+            ...state,
+            // Methods
+            closeSideMenu,
+            openSideMenu,
+        }}
+        >
+            {children}
+        </UIContext.Provider>
+    );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { darkThem, lightThem } from '../themes'
+import { UIProvider } from '../context/ui'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <UIProvider>
     <ThemeProvider theme={darkThem}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
+    </UIProvider>
   )
 }
 
