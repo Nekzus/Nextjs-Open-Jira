51de9151c51315b90122fa323021aa1cf4817483
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-04-04 22:31:18 -0300

    Conexion a MongoDB y configuracion variables de entorno

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,10 @@ import { EntryList, NewEntry } from '../components/ui';
 
 
 const HomePage: NextPage = () => {
+
+  console.log(process.env.NEXT_PUBLIC_CLIENT_KEY);
+
+
   return (
     <Layout title='Home - OpenJira'>
       <Grid container spacing={2}>
